6781ebd37b36c1d67eab4505527ea55d6ad4b085
angry-shark <739012175@qq.com>
2019-01-24 11:15:44 +0800

    add the utils

--- demo/app.vue
+++ demo/app.vue
@@ -1,25 +1,62 @@
 <template>
-    <div>
-        <v-title title="Vue组件化"></v-title>
-        <v-button @click="handleClick">点击按钮</v-button>
-        <p>
-            <img src="./images/image.png" style="width: 200px;">
-        </p>
+    <div class="daily">
+        <div class="daily-menu">
+            <div class="daily-menu-item"
+                :class="{on: type === 'recommend'}">每日推荐</div>
+            <div class="daily-menu-item"
+                :class="{on: type === 'daily'}"
+                @click="showThemes =!showThemes">主题日报</div>
+            <ul v-show="showThemes">
+                <li v-for="(item, index) in themes" :key="index">
+                    <a :class="{on: item.id === themeId && type === 'daily'}"
+                        @click="handleToTheme(item.id)">{{ item.name }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="daily-list">
+            <Item></Item>
+        </div>
+        <daily-arcticle></daily-arcticle>
     </div>
 </template>
 <script>
     import vTitle from './title.vue';
     import vButton from './button.vue';
+    import $ from './libs/utils.js';
 
     export default {
-        components: {
-            vTitle,
-            vButton
+        data(){
+            return{
+                themes:[],
+                showThemes:false,
+                type:'recommend',
+                list:[],
+                themeId:0
+            }
         },
         methods: {
-            handleClick (e) {
-                console.log(e);
+            getThemes(){
+                //axios send the request
+                $.ajax.get('themes').then(res =>{
+                    this.themes = res.others;
+                })
+            },
+            handleToTheme(id){
+                //change the class of menu
+                this.type = 'daily';
+                //set current click subclass theme's id
+                this.themeId = id;
+                //clear the data of the middle column
+                this.list = [];
+                $.ajax.get('theme/' + id).then(res => {
+                    //filter the text whose class is one
+                    this.list = res.stories.filter(item => item.class != 1);
+                })
             }
-        }
+        },
+        mounted() {
+            //when init use
+            this.getThemes();
+        },
     }
 </script>
--- demo/libs/utils.js
+++ demo/libs/utils.js
@@ -1,1 +1,18 @@
-import axios from ''
+import axios from 'axios';
+
+const Utils = {
+    imgPath:"http://127.0.0.1:8011/img",
+    apiPath:"http://127.0.0.1:8010/"
+};
+
+//Ajax general setting
+Utils.ajax = axios.create({
+    baseURL:Utils.apiPath
+});
+
+//add the react filter
+Utils.ajax.interceptors.response.use(res => {
+    return res.data;
+});
+
+export default Utils;
